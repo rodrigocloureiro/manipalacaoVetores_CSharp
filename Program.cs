36ec2cfe3a8314b3ef4ccd31ac942a1488187992
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-11 17:30:35 -0300

    títulos exercícios adicionados

--- Program.cs
+++ Program.cs
@@ -2,6 +2,30 @@
 {
     public static void Main(string[] args)
     {
+        /* 1- Inversão de Array -> Escreva um programa que recebe um array de 10 números inteiros do usuário e os mostra na ordem
+        inversa. */
+        Console.WriteLine("1- Inversão de Array");
 
+        /* 2- Menor e Maior Valor -> Escreva um programa que recebe um array de números inteiros do usuário e determina o menor e o
+        maior número dentro desse array. Não utilize funções prontas para essa finalidade. */
+        Console.WriteLine("\n2- Menor e Maior Valor");
+
+        /* 3- Nome em Posição -> Crie um programa onde o usuário informa 5 nomes. Após inserir todos os nomes, o programa deve pedir
+        ao usuário um índice (entre 0 e 4). O programa deve então mostrar o nome que está na posição correspondente a esse índice. */
+        Console.WriteLine("\n3- Nome em Posição");
+
+        /* 4- Cópia Invertida -> Escreva um programa que receba um array de 5 strings do usuário. Em seguida, copie esse array para
+        outro, mas de forma invertida (o último elemento se torna o primeiro, o penúltimo se torna o segundo, e assim por diante).
+        Mostra o array invertido na tela. */
+        Console.WriteLine("\n4- Cópia Invertida");
+
+        /* 5- Contagem de Caracteres -> Peça ao usuário para inserir uma série de palavras (strings). Armazene essas palavras em um
+        array e, em seguida, calcule e mostre quantos caracteres no total foram inseridos, somando todos os caracteres de todas
+        as palavras. */
+        Console.WriteLine("\n5- Contagem de Caracteres");
+
+        /* 6- Identificando números duplicados -> Escreva um programa que receba 10 números inteiros do usuário e indique se há
+        números duplicados entre eles. */
+        Console.WriteLine("\n6- Identificando números duplicados");
     }
 }
